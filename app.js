11da9c162ee923fe1599fc356cf51ec8aca5b3bd
David Tran <davidlamt@gmail.com>
2016-03-02 14:28:15 -0800

    Changed check in / check out button link

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-<<<<<<< HEAD
 // Initializing Mongoose and the connection
 var mongoose    = require('mongoose');
 mongoose.connect('mongodb://employee:employeered@ds019268.mlab.com:19268/employees_red_wedding');
@@ -33,14 +32,6 @@ app.use(session({ cookie: { maxAge: 60000 },
     secret: 'woot',
     resave: false,
     saveUninitialized: false}));
-
-
-//// Using flash messages
-//app.use(require('connect-flash')());
-//app.use(function (req, res, next) {
-//    res.locals.messages = require('express-messages')(req, res);
-//    next();
-//});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
