2b1c8bdb53277838dbb18f3e1c5079a7ad10d74b
Thanapon Sathirathiwat <tsathira@uci.edu>
2016-03-03 13:10:42 -0800

    - Input validation feature added to form in the main page.  User can only enter number as an ID. - Viewlog page table is now populated with dummy data as a model.  The page is now ready for route implementation. - Minor changes in overall look of the pages.

--- public/ajax/mainscript.js
+++ public/ajax/mainscript.js
@@ -4,32 +4,33 @@
 function main(){
 
     // get login page request to back end
-    $('button#loginBtn').click(function(){
-        var idnumber = $('input#idnumber').val();
-        $.ajax({
-            url: 'http://localhost:3000/home/login',
-            type: 'GET',
-            data: {idnumber: idnumber},
-            success: function(response) {
-                console.log('id: '+idnumber);
-                console.log("Going to homepage...");
-            }
-        });
-    })
+    $('input#idnumber').keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+                // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) ||
+                // Allow: Ctrl+C
+            (e.keyCode == 67 && e.ctrlKey === true) ||
+                // Allow: Ctrl+X
+            (e.keyCode == 88 && e.ctrlKey === true) ||
+                // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+            // let it happen, don't do anything
+            return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
 
-    // get viewlog page request to back end
-    $('button#viewlogBtn').click(function(){
-        var idnumber = $('input#idnumber').val();
-        $.ajax({
-            url: 'http://localhost:3000/viewlog',
-            type: 'GET',
-            data: {idnumber: idnumber},
-            success: function(response) {
-                console.log('id: '+idnumber);
-                console.log("Going to Viewlog page...");
-                //window.location.href ='viewlog';
+    $('button#userCheck').on('click',
+        function(){
+            function pageRedirect() {
+                window.location.href = "/home";
             }
+            console.log('Redirecting to home in 5 secs...');
+            setTimeout(pageRedirect(), 5000);
         });
-    })
 }
 $(document).ready(main);
--- routes/viewlog.js
+++ routes/viewlog.js
@@ -3,12 +3,28 @@
  */
 var express = require('express');
 var router = express.Router();
+var employeeModel = require('../models/employeeSchema');
 
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
+
     var id = req.query.idnumber;
-    console.log('received id: ' + id);
-    res.render('viewlog');
+
+    var output;
+    employeeModel.find({id: id}, function(err, docs){
+
+        console.log("found this docs: "+ docs);
+
+        output =[
+            {starttime: '0:00:00', endtime: '1:11:11', duration: '1:11', date: '3/1/16'},
+            {starttime: '0:00:00', endtime: '1:11:11', duration: '1:11', date: '3/1/16'},
+            {starttime: '0:00:00', endtime: '1:11:11', duration: '1:11', date: '3/1/16'}
+        ];
+        res.render('viewlog', {employeeID: id, employeeInfo: output});
+    });
+
+
 });
 
 module.exports = router;
